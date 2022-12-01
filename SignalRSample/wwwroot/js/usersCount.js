c961b8d8bf095c150139597780aa2c6cf00c31c9
Brian Bentancourt <brian.bentancourt@sonda.com>
2022-12-01 11:23:16 -0300

    connecting from client

---
+++ SignalRSample/wwwroot/js/usersCount.js
@@ -0,0 +1,26 @@
+﻿//create connection
+const connectionUserCount = new signalR.HubConnectionBuilder().withUrl("/hubs/userCount").build()
+
+//connect to methods that hub invokes aka recevie notifications from hub
+connectionUserCount.on("updateTotalViews", (value) => {
+    const newCountSpan = document.getElementById("totalViewsCounter")
+    newCountSpan.innerText = value.toString()
+})
+
+//invoke hub methods aka send notification to hub
+function newWindowLoadedClient() {
+    connectionUserCount.send("NewWindowLoaded")
+}
+
+//start connection
+function fulfilled() {
+    //do something on start
+    console.log("Connection to User Hub Succesful")
+    newWindowLoadedClient()
+}
+
+function rejected() {
+    //rejected logs
+}
+
+connectionUserCount.start().then(fulfilled, rejected)
