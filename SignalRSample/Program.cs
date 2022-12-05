44a656b555bbd80f16e11a4d8c0b0d5285005ede
Brian Bentancourt <brian.bentancourt@sonda.com>
2022-12-05 15:05:49 -0300

    basic chat

--- SignalRSample/Program.cs
+++ SignalRSample/Program.cs
@@ -11,7 +11,7 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(connectionString));
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
-builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = false)
     .AddEntityFrameworkStores<ApplicationDbContext>();
 builder.Services.AddControllersWithViews();
 
@@ -47,5 +47,6 @@ app.MapHub<UserHub>("/hubs/userCount");
 app.MapHub<DeathlyHallowsHub>("/hubs/deathlyhallows");
 app.MapHub<HouseGroupHub>("/hubs/housegroup");
 app.MapHub<NotificationHub>("/hubs/notification");
+app.MapHub<ChatHub>("/hubs/chat");
 
 app.Run();
---
+++ SignalRSample/wwwroot/js/chat.js
@@ -0,0 +1,45 @@
+﻿
+const connectionChat = new signalR
+    .HubConnectionBuilder()
+    .withUrl("/hubs/chat")
+    .build()
+
+document.getElementById("sendMessage").disabled = true
+
+document.getElementById("sendMessage").addEventListener("click", (event) => {
+    const sender = document.getElementById("senderEmail").value
+    const message = document.getElementById("chatMessage").value
+    const receiver = document.getElementById("receiverEmail").value
+
+    if (receiver.length > 0) {
+        //send message to receiver
+        connectionChat.invoke("SendMessageToReciever", sender, receiver, message).catch((err) => console.error(err))
+    } else {
+        //send message to all users
+        connectionChat.send("SendMessageToAll", sender, message).catch((err) => console.error(err))
+    }
+
+    event.preventDefault()
+
+})
+
+
+connectionChat.on("MessageReceived", (user, message) => {
+    const li = document.createElement("li")
+    document.getElementById("messagesList").appendChild(li)
+    li.textContent = `${user} - ${message}`
+})
+
+
+//start connection
+
+function fulfilled() {
+    //do something on start
+    console.log("Connection to Chat Hub Succesful")
+    document.getElementById("sendMessage").disabled = false
+}
+
+function rejected() {
+    //rejected logs
+}
+connectionChat.start().then(fulfilled, rejected)
