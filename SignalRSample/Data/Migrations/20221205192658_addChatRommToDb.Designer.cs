f85acae259c8758201fd3627a52a6ae5cfe0e1dd
Brian Bentancourt <brian.bentancourt@sonda.com>
2022-12-05 16:27:10 -0300

    add-migration addChatRommToDb

--- SignalRSample/Data/Migrations/20221205192658_addChatRommToDb.Designer.cs
+++ SignalRSample/Data/Migrations/20221205192658_addChatRommToDb.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using SignalRSample.Data;
 
@@ -11,9 +12,11 @@ using SignalRSample.Data;
 namespace SignalRSample.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20221205192658_addChatRommToDb")]
+    partial class addChatRommToDb
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
